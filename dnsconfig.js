4efff268766f3bc1cf7bebd79884cd64b94cac40
Manuel Hutter <manuel@hutter.io>
2018-01-30 21:25:43 +0100

    Added node0, node1

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,6 +5,10 @@ var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD')
 
 D('mhnet.ch', REG_NONE, DnsProvider(GCLOUD),
   {'ns_ttl': '21600'},
-  A('master0', '94.130.184.104'),
-  AAAA('master0', '2a01:4f8:1c0c:445f::')
+     A('master0', '94.130.184.104'),
+  AAAA('master0', '2a01:4f8:1c0c:445f::'),
+     A('node0', '195.201.21.131'),
+  AAAA('node0', '2a01:4f8:1c0c:4d8e::'),
+     A('node1', '195.201.21.132'),
+  AAAA('node1', '2a01:4f8:1c0c:4d8f::')
 )
