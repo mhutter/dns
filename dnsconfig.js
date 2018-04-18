21cef0fc0b5ce83acb39fe977696a8ec26a8a998
Manuel Hutter <manuel@hutter.io>
2018-04-18 02:01:47 +0200

    Fix AAAA records

--- dnsconfig.js
+++ dnsconfig.js
@@ -6,15 +6,15 @@ var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD')
 D('mhnet.ch', REG_NONE, DnsProvider(GCLOUD),
   {'ns_ttl': '21600'},
      A('controller-0', '88.99.12.218'),
-  AAAA('controller-0', '2a01:4f8:1c17:5197::'),
+  AAAA('controller-0', '2a01:4f8:1c17:5197::1'),
      A('controller-1', '78.47.89.155'),
-  AAAA('controller-1', '2a01:4f8:1c17:46e4::'),
+  AAAA('controller-1', '2a01:4f8:1c17:46e4::1'),
      A('controller-2', '78.47.109.131'),
-  AAAA('controller-2', '2a01:4f8:1c17:4ab1::'),
+  AAAA('controller-2', '2a01:4f8:1c17:4ab1::1'),
      A('worker-0', '88.99.12.227'),
-  AAAA('worker-0', '2a01:4f8:1c17:4cfd::'),
+  AAAA('worker-0', '2a01:4f8:1c17:4cfd::1'),
      A('worker-1', '78.47.89.156'),
-  AAAA('worker-1', '2a01:4f8:1c17:48aa::'),
+  AAAA('worker-1', '2a01:4f8:1c17:48aa::1'),
      A('worker-2', '78.47.109.129'),
-  AAAA('worker-2', '2a01:4f8:1c17:4987::')
+  AAAA('worker-2', '2a01:4f8:1c17:4987::1')
 )
