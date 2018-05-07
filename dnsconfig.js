c986b8e408b0cfc8fc9be6173cc5eec14febd415
Manuel Hutter <manuel@hutter.io>
2018-05-07 11:52:24 +0200

    Add cname for shop

--- dnsconfig.js
+++ dnsconfig.js
@@ -4,5 +4,6 @@ var REG_NONE = NewRegistrar('none', 'NONE')
 var GCLOUD = NewDnsProvider('gcloud', 'GCLOUD')
 
 D('mhnet.ch', REG_NONE, DnsProvider(GCLOUD),
-  {'ns_ttl': '21600'}
+  {'ns_ttl': '21600'},
+  CNAME('shop', '0ac90246-463a-4625-a0eb-9a1f417f8b40-swiss21-commerce-int.app.abasky.net.')
 )
