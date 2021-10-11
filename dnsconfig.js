4e4a595cca83fe3a771e3fb34d70557c8329870d
Manuel Hutter <manuel@hutter.io>
2021-10-11 23:57:39 +0200

    the-voice.cc: Move mail forwarding to gandi
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- dnsconfig.js
+++ dnsconfig.js
@@ -8,10 +8,9 @@ D('the-voice.cc', REG_GANDI, DnsProvider(CLOUDFLARE),
   CNAME('www', 'the-voice.cc.', CF_PROXY_ON),
 
   // Mail setup
-  CNAME('autoconfig', 'maildiscovery.cyon.ch.'),
-  MX('@', 0, 's037.cyon.net.'),
-  TXT('default._domainkey', 'v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwn0X3QDbEG4mvj/LzoE2v5jRyzl55eu7o3jafPVqDizHI/UD7zyml/LFzoClKW43nHfmb8J6g/aiyYM7qtDGAqMGful4zkZmDHx8Gxuk3A/NCgEl49XO3ZzHSFUFHSpwwU2pt2SW3/9l1SZMo+cFlP4fE1hDvEZA9QcLbDw0lfm9WgcWP8e8nQTxr+Icw8+rzkXOQkvZVMmJqWdppg1EdlJ4HNzjm7kErhJv0KgOHgOsiczghcRBNq9t2o2NiTIizhZyoRUrvuWnHBb6+gyW+mMXNuy/KkSx4UvBy9r1YyX6Cl+6tgacH7lZHhyivW3f6T3uoqsHhW4vHnoRCeRwbwIDAQAB;'),
-  TXT('@', 'v=spf1 include:spf.protection.cyon.net -all'),
+  MX('@', 10, 'spool.mail.gandi.net.'),
+  MX('@', 50, 'fb.mail.gandi.net.'),
+  TXT('@', 'v=spf1 include:_mailcust.gandi.net ?all'),
 
   // Redirects
   CF_REDIRECT('www.the-voice.cc/*', 'https://the-voice.cc/$1')
