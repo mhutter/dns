8675dd10e21cf9700efbce5cd968cd096ded29ec
Manuel Hutter <manuel@hutter.io>
2021-10-13 14:38:34 +0200

    Fix mhutter.net DKIM
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- dnsconfig.js
+++ dnsconfig.js
@@ -10,14 +10,5 @@ var GANDI_MX = [
   TXT('@', 'v=spf1 include:_mailcust.gandi.net ?all'),
 ];
 
-var FASTMAIL_MX = [
-  MX('@', 10, 'in1-smtp.messagingengine.com.'),
-  MX('@', 20, 'in2-smtp.messagingengine.com.'),
-  CNAME('fm1._domainkey', 'fm1.hutter.io.dkim.fmhosted.com.'),
-  CNAME('fm2._domainkey', 'fm2.hutter.io.dkim.fmhosted.com.'),
-  CNAME('fm3._domainkey', 'fm3.hutter.io.dkim.fmhosted.com.'),
-  TXT('@', 'v=spf1 include:spf.messagingengine.com ?all'),
-];
-
 // Zones
 require_glob('./zones/');
--- zones/mhutter.net.js
+++ zones/mhutter.net.js
@@ -3,7 +3,12 @@ D('mhutter.net', REG_GANDI, DnsProvider(CLOUDFLARE),
   CNAME('www', '@', CF_PROXY_ON),
 
   // Mail
-  FASTMAIL_MX,
+  MX('@', 10, 'in1-smtp.messagingengine.com.'),
+  MX('@', 20, 'in2-smtp.messagingengine.com.'),
+  CNAME('fm1._domainkey', 'fm1.mhutter.net.dkim.fmhosted.com.'),
+  CNAME('fm2._domainkey', 'fm2.mhutter.net.dkim.fmhosted.com.'),
+  CNAME('fm3._domainkey', 'fm3.mhutter.net.dkim.fmhosted.com.'),
+  TXT('@', 'v=spf1 include:spf.messagingengine.com ?all'),
 
   // Redirects
   CF_REDIRECT('mhutter.net/*', 'https://mhu.dev/$1'),
