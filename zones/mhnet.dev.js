7cbaeaf7918ad5b70c61ff523065659575d70f99
Manuel Hutter <manuel@hutter.io>
2021-11-25 12:12:29 +0100

    Add s3.mhnet.dev
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- zones/mhnet.dev.js
+++ zones/mhnet.dev.js
@@ -7,5 +7,9 @@ D('mhnet.dev', REG_GANDI, DnsProvider(CLOUDFLARE),
   CNAME('s1._domainkey', 's1.domainkey.u22452480.wl231.sendgrid.net.'),
   CNAME('s2._domainkey', 's2.domainkey.u22452480.wl231.sendgrid.net.'),
 
+  // mhu0.mhnet.dev
+  A('s3', '168.119.153.20', CF_PROXY_ON),
+  CNAME('s3-console', 's3.mhnet.dev.', CF_PROXY_ON),
+
   CAA('@', 'issue', 'letsencrypt.org')
 );
