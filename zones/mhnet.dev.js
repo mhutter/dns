04446ffa62c2aedb112cd54744758390e98e1b6d
Manuel Hutter <manuel@hutter.io>
2021-11-26 18:53:04 +0100

    Remove mhu0 POC
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- zones/mhnet.dev.js
+++ zones/mhnet.dev.js
@@ -1,20 +1,28 @@
-var MHU_IP = '168.119.153.20';
+var HOME = '85.195.254.187';
+var RASPUTIN_APPS = [
+  'minio',
+  'objects',
+  'registry',
+  'transmission',
+  'vaultwarden',
+];
 
 D('mhnet.dev', REG_GANDI, DnsProvider(CLOUDFLARE),
-  A('@', '85.195.254.187'),
-  CNAME('*', '@'),
+  // Rasputin
+  RASPUTIN_APPS.map(function(e) {
+    return A(e, HOME, CF_PROXY_ON)
+  }),
 
   // Sendgrid
   CNAME('em5597', 'u22452480.wl231.sendgrid.net.'),
   CNAME('s1._domainkey', 's1.domainkey.u22452480.wl231.sendgrid.net.'),
   CNAME('s2._domainkey', 's2.domainkey.u22452480.wl231.sendgrid.net.'),
 
-  // mhu0.mhnet.dev
-  A('mhu0',       MHU_IP, CF_PROXY_ON),
-  A('prometheus', MHU_IP, CF_PROXY_ON),
-  A('registry',   MHU_IP, CF_PROXY_ON),
-  A('s3',         MHU_IP, CF_PROXY_OFF),
-  A('s3-console', MHU_IP, CF_PROXY_ON),
+  // Mail
+  TXT('_dmarc', 'v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;'),
+  TXT('*._domainkey', 'v=DKIM1; p='),
+  TXT('@', 'v=spf1 -all'),
 
+  // CAA
   CAA('@', 'issue', 'letsencrypt.org')
 );
