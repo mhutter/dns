9be88fd9755e9d4ef684d6dc98428f99599cef39
Manuel Hutter <manuel@hutter.io>
2022-06-30 11:15:41 +0200

    feat!: Remove IPv4 records for home
    They are not fixed
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- zones/mhnet.dev.js
+++ zones/mhnet.dev.js
@@ -1,4 +1,3 @@
-var HOME = '85.195.254.189';
 var HOMEv6 = '2a02:168:81cd::ea02:aff2:567d:9bd9';
 var RASPUTIN_APPS = [
   'minio',
@@ -10,9 +9,6 @@ var RASPUTIN_APPS = [
 
 D('mhnet.dev', REG_GANDI, DnsProvider(CLOUDFLARE),
   // Rasputin
-  RASPUTIN_APPS.map(function (e) {
-    return A(e, HOME, CF_PROXY_ON)
-  }),
   RASPUTIN_APPS.map(function (e) {
     return AAAA(e, HOMEv6, CF_PROXY_ON)
   }),
