c00ba0784b21e77f5cb38a48e77145f1be217757
Manuel Hutter <manuel@hutter.io>
2024-01-12 16:21:27 +0100

    Mailjet validation
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- zones/mhnet.app.js
+++ zones/mhnet.app.js
@@ -8,6 +8,9 @@ D('mhnet.app', REG_NONE,
   CNAME('s2._domainkey', 's2.domainkey.u22452480.wl231.sendgrid.net.'),
   TXT('_dmarc', 'v=DMARC1; p=none;'),
 
+  // Mailjet
+  TXT('mailjet._6a0947b7', '6a0947b7fb1830aea28a9c96352a1e5b'),
+
   // CAA
   CAA('@', 'issue', 'letsencrypt.org')
 );
