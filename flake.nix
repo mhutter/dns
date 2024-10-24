7e81e532faf6482bdb663a88634bd91b83d84814
Manuel Hutter <manuel@hutter.io>
2024-10-24 10:43:48 +0200

    bump tools
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
